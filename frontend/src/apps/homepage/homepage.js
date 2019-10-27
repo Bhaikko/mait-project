aca0277d59b39bce1dfbee83e717bc41943df9af
Bhaikko <siddhartha.pawar17@gmail.com>
2019-10-27 15:11:55 +0530

    Some changes to homepage

--- frontend/src/apps/homepage/homepage.js
+++ frontend/src/apps/homepage/homepage.js
@@ -4,6 +4,7 @@ import classes from './homepage.css';
 import Logo from './../../components/Logo/Logo';
 import Infos from './../../components/Homepage/Infos/Infos';
 import HomepageCard from './../../components/Homepage/HomepageCard/HomepageCard';
+import Button from './../../components/UI/Button/Button';
 
 class Homepage extends Component {
     render () {
@@ -11,16 +12,52 @@ class Homepage extends Component {
             <div className={classes.Homepage}>
                 <div className={classes.CardsContainer}>
                     <HomepageCard style={{ backgroundColor: "#9D354B"}} >
-                        <Logo />
+                        <Logo style={{fontSize: 40}} />
                         <Infos />
                     </HomepageCard>
                     <HomepageCard style={{ backgroundColor: "#2D4571"}} >
-                        <Logo />
-                        <Infos />
+                        <div>
+                            <Logo style={{fontSize: 40}} />
+                            <Button style={{marginLeft: 50}}>Login</Button>
+                        </div>
+                        <div style={{fontSize: 20, color: "white", marginTop: 20}}>
+                            A place to talk and have <br />
+                            community-based discussions <br />
+                            with like-minded people. <br />
+                        </div>
+                        <div style={{fontSize: 20, color: "white", marginTop: 20}}>
+                            Join Mait Talks today.
+                            <Button style={{marginLeft: 50}}>Sign Up</Button>
+                        </div>
                     </HomepageCard>
                     <HomepageCard style={{ backgroundColor: "#2D4571"}} >
-                        <Logo />
-                        <Infos />
+                        <div className={classes.PeopleTitle}>The People</div>
+                        <div className={classes.PeopleContainer}>
+                            <div>
+                                <div className={classes.PeopleBox}>
+                                    Sarthak Mittal
+                                </div>
+                                <div className={classes.PeopleBox}>
+                                    Web Developer
+                                </div>
+                            </div>
+                            <div>
+                                <div className={classes.PeopleBox}>
+                                    Khush Khanna
+                                </div>
+                                <div className={classes.PeopleBox}>
+                                    UI/UX Designer
+                                </div>
+                            </div>
+                            <div>
+                                <div className={classes.PeopleBox}>
+                                    Siddharth Pawar
+                                </div>
+                                <div className={classes.PeopleBox}>
+                                    Web Developer
+                                </div>
+                            </div>
+                        </div>
                     </HomepageCard>
                     <HomepageCard style={{ backgroundColor: "#9D354B"}} >
                         <Logo />
