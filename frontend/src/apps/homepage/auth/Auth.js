c6b32dc7feb6dd6e45ff14156ee06ecc178b8c67
Sarthak Mittal <sam.mittal.2106@gmail.com>
2019-11-05 22:19:53 +0530

    auth page fixes

--- frontend/src/apps/homepage/auth/Auth.js
+++ frontend/src/apps/homepage/auth/Auth.js
@@ -8,20 +8,32 @@ import SignupForm from './SignupForm/SignupForm';
 
 
 const Login = props => {
+    const myRef = React.useRef(null);
+    const scrollToRef = (ref) => window.scrollTo({left : 0, top : ref.current.offsetTop, behavior : 'smooth'});
+    function handleClick() {
+        scrollToRef(myRef);
+      }
     return (
         <div className={classes.Auth}>
             <HomepageCard style={{ 
                 backgroundColor: "#9D354B",
-                padding: 100
-            }} class="Card">
+                padding: 10
+            }}>
                 <LoginForm />
+                
+                <div className={classes.signupLink} onClick={handleClick}>
+                    Sign Up
+                </div>
             </HomepageCard>
+            
             <HomepageCard style={{ 
                 backgroundColor: "#2D4571",
-                padding: 100
-            }} class="Card">
+                padding: 10
+            }} refprop={myRef}>
                 <SignupForm />
+                
             </HomepageCard>
+            
         </div>
     );
 }
--- frontend/src/components/Homepage/HomepageCard/HomepageCard.js
+++ frontend/src/components/Homepage/HomepageCard/HomepageCard.js
@@ -8,7 +8,7 @@ const HomepageCard = props => {
         styles.order = 0;
     }
     return (
-        <div className={classes.Card} style={styles}>
+        <div className={classes.Card} style={styles} ref={props.refprop}>
             {props.children}
         </div>
     );
